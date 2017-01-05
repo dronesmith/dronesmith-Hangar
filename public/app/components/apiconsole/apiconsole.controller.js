9db93e4badc1c69dc55cd64570e1933eddd90b66
pfjare <paulf@dronesmith.io>
2017-01-04 18:23:34 -0800

    improved layout, added error messages

--- public/app/components/apiconsole/apiconsole.controller.js
+++ public/app/components/apiconsole/apiconsole.controller.js
@@ -17,11 +17,11 @@ angular
   .controller('APIConsoleCtrl', function ($scope, $rootScope, $interval, API) {
 
 
-        $scope.selectHeight = {"height": "5vh"};
-        $scope.opened = false;
-        $scope.selectGlyph = "glyphicon-triangle-top";
+    // TODO: improve positioning of status text
+    // TODO: validate url on change
+    // TODO: remind user to select drone if {drone} is in url
+    // TODO: change color of status text based on error or not, maybe colored circle
 
-        $scope.datalogs = [];
 
     $scope.methods = [
         {id: "1", name: "GET"},
@@ -50,17 +50,32 @@ angular
       }
     });
 
-    $scope.validJSON = true;
-
     $scope.testJSON = function(json) {
-      var validJSON;
-      try {
-        validJSON = JSON.parse(json);
-        $scope.validJSON = true;
-      } catch(e) {
-        $scope.validJSON = false;
+      if (json === "") {
+        $scope.consoleForm.body.$setValidity('json', true);
+
+      } else {
+        try {
+          JSON.parse(json);
+          $scope.consoleForm.body.$setValidity('json', true);
+        } catch(e) {
+          $scope.consoleForm.body.$setValidity('json', false);
+        }
       }
+
     }
+
+    $scope.bodyDisabled = true;
+
+    $scope.testMethod = function(method) {
+      if(method === "POST" || method === "PUT" || method === "DELETE" ){
+        $scope.bodyDisabled = false;
+      } else {
+        $scope.bodyDisabled = true;
+      }
+
+    }
+    $scope.requestIconColor = "#d3d3d3";
 
     $scope.sendRequest = function(method, drone, urlportion, body) {
       // Clear Response text
@@ -72,22 +87,86 @@ angular
         urlportion = urlportion.replace(/({drone})/, $scope.request.drone.name);
       }
 
-
       // Send API request
       API.sendRequest(method, drone, urlportion, body, function(response){
         $scope.response = response;
+
+        // set request status icon
+        if(Number(String(response.status).charAt(0)) === 4){
+          $scope.requestIconColor = "#e74c3c";
+        } else {
+          $scope.requestIconColor = "#a0d468";
+        }
+
       });
     }
-    
+<<<<<<< HEAD
+
+=======
+
+
+
+
+    $scope.selectHeight = {"height": "28px"};
+    $scope.opened = false;
+    $scope.selectGlyph = "glyphicon glyphicon-chevron-up";
+
+    $scope.datalogs = [];
+
+    $rootScope.$on('telem:update', function(ev, data) {
+      $scope.datalogs = [];
+      var logs = API.getLog();
+
+      for (var i = logs.length-1; i >= 0; --i) {
+        var log = logs[i];
+
+        switch (log.method) {
+          case 'GET': log.methodClass = 'label-success'; break;
+          case 'POST': log.methodClass = 'label-info'; break;
+          case 'PUT': log.methodClass = 'label-default'; break;
+          case 'DELETE': log.methodClass = 'label-danger'; break;
+        }
+
+        $scope.datalogs.push(log);
+      }
+    });
+
+    // $scope.datalogs = {};
+    // $scope.datalogs.get = function(index, count, success) {
+    //
+    //   $interval(function() {
+    //     var logs = API.getLog();
+    //     var chunk = [];
+    //
+    //     for (var i = index; i < logs.length && i <= index + count - 1; i++) {
+    //       var log = logs[i];
+    //
+    //       if (log) {
+    //         switch (log.method) {
+    //           case 'GET': log.methodClass = 'label-success'; break;
+    //           case 'POST': log.methodClass = 'label-info'; break;
+    //           case 'PUT': log.methodClass = 'label-default'; break;
+    //           case 'DELETE': log.methodClass = 'label-danger'; break;
+    //         }
+    //
+    //         chunk.push(log);
+    //       }
+    //     }
+    //
+    //     success(chunk);
+    //   }, 2000);
+    // };
+
+>>>>>>> improved layout, added error messages
     $scope.toggleView = function() {
       $scope.opened = !$scope.opened;
 
       if (!$scope.opened) {
-        $scope.selectHeight = {"height": "5vh"};
-        $scope.selectGlyph = "glyphicon-triangle-top";
+        $scope.selectHeight = {"height": "28px"};
+        $scope.selectGlyph = "glyphicon glyphicon-chevron-up";
       } else {
         $scope.selectHeight = {"height": "40vh"};
-        $scope.selectGlyph = "glyphicon-triangle-bottom";
+        $scope.selectGlyph = "glyphicon glyphicon-chevron-down";
       }
     }
   })
