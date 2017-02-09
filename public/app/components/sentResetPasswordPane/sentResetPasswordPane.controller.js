cdcf272b2a0d465e47705bb6f41a962e6f51c4a3
pfjare <paulf@dronesmith.io>
2017-02-09 11:17:36 -0800

    use uisref instead of on click function and fix button style

--- public/app/components/sentResetPasswordPane/sentResetPasswordPane.controller.js
+++ public/app/components/sentResetPasswordPane/sentResetPasswordPane.controller.js
@@ -5,11 +5,7 @@ angular
   .module('ForgeApp')
   .controller('SentResetPasswordPaneCtrl', function ($scope, $state, Session, $stateParams) {
 
-        console.log($stateParams);
     $scope.email = $stateParams.email;
 
-    $scope.gotoLogin = function() {
-      $state.go('login');
-    }
 
   });
