3e935c70ca24dec9fe80b63ad53ddc8c250dd020
pfjare <paulf@dronesmith.io>
2017-02-12 11:38:42 -0800

    improve loook of signup and verify email messages

--- public/app/components/validateEmailPane/validateEmailPane.controller.js
+++ public/app/components/validateEmailPane/validateEmailPane.controller.js
@@ -5,15 +5,16 @@ angular
   .module('ForgeApp')
   .controller('ValidateEmailPaneCtrl', function ($scope, $state, $stateParams, Session) {
     $scope.error = $stateParams.error;
-    $scope.message = " "
+    $scope.message = ""
+    $scope.messageHeader = ""
 
     Session
       .validateEmail.send($stateParams)
       .$promise.then(function(data) {
+        $scope.messageHeader = "Thank you!";
         $scope.message = "Your email has been confirmed. We sent you an email containing your API key.";
       }, function(data) {
         $scope.error = data.data.error;
-        //$state.go('forgotPassword', {error: $scope.error});
       });
 
 
